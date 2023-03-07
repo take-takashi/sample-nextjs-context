f2a89568136a2daed73a85f6b1ebf66bbb589864
kaerunrun <take.t.public@gmail.com>
2023-03-07 15:11:41 +0000

    ダッシュボード修正

--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -1,21 +1,42 @@
 import { NextPage } from "next";
 import Link from "next/link";
 
+type Page = {
+  page: string; // ページのパス
+  description: string; // ページの説明
+};
+
+const links: Page[] = [
+  {
+    page: "./readGlobalVar",
+    description: "Context APIを使ってグローバルなstateを読み取るサンプル",
+  },
+  {
+    page: "./writeGlobalVar",
+    description:
+      "【失敗】Context APIを使ってグルーバルなstateを書き込むサンプル",
+  },
+  {
+    page: "./sampleContextPage",
+    description: "Context APIを使ってグローバルなstateを読み書きするサンプル",
+  },
+  {
+    page: "./sampleContextsPage",
+    description:
+      "【結論】Context APIを使って複数のグローバルなstateを読み書きするサンプル",
+  },
+];
+
 const Home: NextPage = () => {
   return (
     <main>
-      <p>
-        <Link href="./readGlobalVar">ReadGlobalVar</Link>
-      </p>
-      <p>
-        <Link href="./writeGlobalVar">WriteGlobalVar</Link>
-      </p>
-      <p>
-        <Link href="./sampleContextPage">SampleContextPage</Link>
-      </p>
-      <p>
-        <Link href="./sampleContextsPage">SampleContextsPage</Link>
-      </p>
+      {links.map((page) => {
+        return (
+          <p key={page.page}>
+            <Link href={page.page}>{page.description}</Link>
+          </p>
+        );
+      })}
     </main>
   );
 };
